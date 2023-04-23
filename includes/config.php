0e3dee986ac239cc4d67398d5e5d832d5b86d546
Allan Valencia <allancarlosmanuelva@gmail.com>
2023-04-22 22:08:24 -0600

    App version 1

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+
+<?php
+/*Clase para realizar la conexion a la BD MYSQL*/
+  define( 'DB_HOST', 'db-analisis1.cnmoran6ej8o.us-east-2.rds.amazonaws.com' );          // Asignar host
+  define( 'DB_USER', 'root' );             // Asignar usuario 
+  define( 'DB_PASS', 'Allan53540$' );             //Asignar password
+  define( 'DB_NAME', 'inventarios' );        // Asignar nombre de la base de datos
+
+?>
---
+++ includes/database.php
@@ -0,0 +1,111 @@
+<?php
+require_once(LIB_PATH_INC.DS."config.php");
+
+class MySqli_DB {
+
+    private $con;
+    public $query_id;
+
+    function __construct() {
+      $this->db_connect();
+    }
+
+/*--------------------------------------------------------------*/
+/* Funcion para abrir la conexion hacia la BASE DE DATOS
+/*--------------------------------------------------------------*/
+public function db_connect()
+{
+  $this->con = mysqli_connect(DB_HOST,DB_USER,DB_PASS);
+  if(!$this->con)
+         {
+           die(" Database connection failed:". mysqli_connect_error());
+         } else {
+           $select_db = $this->con->select_db(DB_NAME);
+             if(!$select_db)
+             {
+               die("Failed to Select Database". mysqli_connect_error());
+             }
+         }
+}
+/*--------------------------------------------------------------*/
+/* Funcion para cerrar la conexion hacia la BASE DE DATOS
+/*--------------------------------------------------------------*/
+
+public function db_disconnect()
+{
+  if(isset($this->con))
+  {
+    mysqli_close($this->con);
+    unset($this->con);
+  }
+}
+/*--------------------------------------------------------------*/
+/* Funcion para poder ejecutar los querys de MYSQL
+/*--------------------------------------------------------------*/
+public function query($sql)
+   {
+
+      if (trim($sql != "")) {
+          $this->query_id = $this->con->query($sql);
+      }
+      if (!$this->query_id)
+        // Unicamente cuando el despliegue sea dev
+              die("Error en esta consulta :<pre> " . $sql ."</pre>");
+       // para produccion
+        //  die("Error on Query");
+
+       return $this->query_id;
+
+   }
+
+/*--------------------------------------------------------------*/
+/* Funcion de utilidades
+/*--------------------------------------------------------------*/
+public function fetch_array($statement)
+{
+  return mysqli_fetch_array($statement);
+}
+public function fetch_object($statement)
+{
+  return mysqli_fetch_object($statement);
+}
+public function fetch_assoc($statement)
+{
+  return mysqli_fetch_assoc($statement);
+}
+public function num_rows($statement)
+{
+  return mysqli_num_rows($statement);
+}
+public function insert_id()
+{
+  return mysqli_insert_id($this->con);
+}
+public function affected_rows()
+{
+  return mysqli_affected_rows($this->con);
+}
+/*--------------------------------------------------------------*/
+ /* Funcion para remover los escapes o
+ /* caracteres en nuestra consulta
+ /*--------------------------------------------------------------*/
+ public function escape($str){
+   return $this->con->real_escape_string($str);
+ }
+/*--------------------------------------------------------------*/
+/* Funcion Loop de una sola instancia
+/*--------------------------------------------------------------*/
+public function while_loop($loop){
+ global $db;
+   $results = array();
+   while ($result = $this->fetch_array($loop)) {
+      $results[] = $result;
+   }
+ return $results;
+}
+
+}
+
+$db = new MySqli_DB();
+
+?>
